b075d3a3d7180640b7b12515d5a930c8ada0fe21
sherwin-casem <162948581+ad-larsen-co@users.noreply.github.com>
2023-06-01 23:31:53 +0200

    Add more styles

--- app/page.js
+++ app/page.js
@@ -3,13 +3,33 @@ import Link from 'next/link';
 import { products } from '../database/products';
 import styles from './page.module.scss';
 
-function HomePage() {
+export default function HomePage() {
   return (
-    <div style={{ backgroundColor: 'green' }}>
-      <h1>Best burgers in town</h1>
-      <Image src="/img/homepage.jpg" alt="Homepage" width={500} height={300} />
-    </div>
+    <>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'left',
+        }}
+      >
+        <Image src="/img/homepage.jpg" width="500" height="300" />
+      </div>
+      <p>Life is too short for self-hatred and celery sticks.</p>
+
+      <input type="button" value="Menu" class="center"></input>
+    </>
   );
 }
 
-export default HomePage;
+{
+  /* // function HomePage() {
+  return (
+    <div>
+      <h1>Best burgers in town</h1>
+      <style>.imgMain margin-left:auto; margin-right:auto;</style>
+      <Image src="/img/homepage.jpg" class="imgMain"></Image> width={500}{' '}
+      height={300}
+    </div>
+  );
+} */
+}
--- database/products.js
+++ database/products.js
@@ -5,7 +5,6 @@ export const products = [
     firstName: 'Luther',
     price: 19,
     type: 'burger',
-    accessory: 'olive',
     text: "With more than 1000 calories and usually over 45 grams of fat per serving, Luther burger is one of the world's unhealthiest burgers, but its flavors and a unique combination of ingredients make up for it. The standard burger bun is replaced by a sliced, sugar-glazed donut (usually a Krispy Kreme).Aside from that, Luther is basically a cheeseburger topped with bacon, and it is typically served without vegetables or condiments. Many people believe that it was invented at Mulligan's Bar in Georgia, when the cook ran out of standard hamburger buns and used donuts instead.",
   },
   {
@@ -13,24 +12,20 @@ export const products = [
     firstName: 'Ainsworth',
     price: 17,
     type: 'burger',
-    accessory: 'onion',
     text: 'The Mac & Cheese Burger is a thing of pure insanity. It features a panko-crusted mac & cheese patty, topped with a beef patty, topped with more mac & cheese. The cheese makes the patty hard to find, but once beef meets tongue it is a pure bliss. The bun was shockingly able to hold its own against the deluge of melted cheese, noodles and meat..',
   },
   {
     id: 3,
     firstName: 'Mexican-chorizo-garlic-shrimp',
     price: 18,
-    type: 'cheeseburger',
-    accessory: 'cheese spread',
+    type: 'burger',
     text: "Pimento cheeseburger is a traditional American version of a cheesburger originating from the South, and it's especially popular in South Carolina. It consists of soft burger buns, grilled burger patties, and pimento cheese spread. The pimento spread is made with a base of grated cheddar, mayonnaise, and pimento peppers.Once assembled, the pimento cheeseburger can be eaten as it is, although some people prefer additional toppings such as lettuce, fried onions, or tomatoes, but those are completely optional.",
   },
   {
     id: 4,
-
     firstName: 'Bacon-Whiskey',
     price: 18,
     type: 'burger',
-    accessory: 'sugar-glazed donuts',
     text: 'Poached burger is an American burger variety originating from Prairie du Chien, Wisconsin, and it’s made at Pete’s Hamburgers, the only place to get this burger. The meat is poached in shallow, hot water because the owner, Pete Gokey once started pouring water on the burgers to keep them moist, as the patties that sat too long would dry out.The water pan in which the meat cooks is filled with sliced onions that flavor the liquid and the patties. Once done, the meat is placed into a bakery roll with a scoop of the onions, and the burgers are usually finished with a squirt of horseradish mustard.',
   },
 ];
