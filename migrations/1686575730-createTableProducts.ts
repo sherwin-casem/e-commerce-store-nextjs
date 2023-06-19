0072b0b2bcbcbfe3980e19bc6bd43a2561df98d9
sherwin-casem <162948581+ad-larsen-co@users.noreply.github.com>
2023-06-19 16:01:36 +0200

    Add log in

--- migrations/1686575730-createTableProducts.ts
+++ migrations/1686575730-createTableProducts.ts
@@ -1,20 +1,27 @@
 import { Sql } from 'postgres';
 
+export type Product = {
+  id: number;
+  firstName: string;
+  price: number;
+  type: string;
+  text: string;
+};
+
 export async function up(sql: Sql) {
   await sql`
-CREATE TABLE products (
-id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-first_name varchar(30) NOT NULL,
-price varchar(30) NOT NULL,
-type varchar(30) NOT NULL,
-text varchar(800)
-)
-`;
+    CREATE TABLE products (
+      id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+      first_name varchar(30) NOT NULL,
+      price varchar(30) NOT NULL,
+      type varchar(30) NOT NULL,
+      text varchar(800)
+    )
+  `;
 }
 
 export async function down(sql: Sql) {
   await sql`
-  DROP TABLE products
-
-`;
+    DROP TABLE products
+  `;
 }
