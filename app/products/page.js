a20cb552bcff5651f3313be3d194c7f091d5bfe1
sherwin-casem <162948581+ad-larsen-co@users.noreply.github.com>
2023-06-28 17:30:00 +0200

    Modify styling

--- app/products/page.js
+++ app/products/page.js
@@ -25,7 +25,7 @@ export default async function ProductsPage() {
                 height={150}
                 priority
               />
-              <p> {product.text}</p>
+              <p>{product.text}</p>
             </div>
           );
         })}
